537eec33292cb53094f22f98cb76da4e5fff74bf
Jean-Roch Meurisse <jean-roch.meurisse@fundp.ac.be>
2017-01-03 15:39:28 +0100

    add settings file and associated lib methods

--- lib.php
+++ lib.php
@@ -20,3 +20,123 @@
  * @copyright  2016 - Cellule TICE - Unversite de Namur
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Function that generates a chunk of SCSS to be prepended to the main scss file.
+function theme_mwar_get_pre_scss($theme) {
+    // 1. To define our own configurable scss variables use the code below and comment code under 2.
+    global $CFG;
+
+    $scss = '';
+    $configurable = [
+        // Config key => [scss variableName1, ...].
+        'brandcolor' => ['brand-primary'],
+    ];
+
+    // Prepend variables first.
+    foreach ($configurable as $configkey => $targets) {
+        $value = isset($theme->settings->{$configkey}) ? $theme->settings->{$configkey} : null;
+        if (empty($value)) {
+            continue;
+        }
+
+        array_map(function($target) use (&$scss, $value) {
+            $scss .= '$' . $target . ': ' . $value . ";\n";
+        }, (array) $targets);
+    }
+
+    // Then prepend pre-scss code added in theme config.
+    if (!empty($theme->settings->scsspre)) {
+        $scss .= $theme->settings->scsspre;
+    }
+    return $scss;
+
+    // 2. To use 'brandcolor' and 'scsspre' variables from boost theme. Value is therefore defined in boost theme and not in our custom one. To use this code uncomment below and comment code under 1.
+    /*$theme = theme_config::load('boost');
+    return theme_boost_get_pre_scss($theme);*/
+}
+
+// Function that generates a chunk of SCSS code to be added to the end of the main scss file.
+function theme_mwar_get_extra_scss($theme) {
+    // 1. To define our own extra scss variable. To use it uncomment the code below and comment under 2.
+    return !empty($theme->settings->scss) ? $theme->settings->scss : '';
+    // 2. To use 'scss' variable from boost theme. (See theme_mwar_get_pre_scss -> 2). To use it uncomment the code below and uncomment under 1.
+    /*$theme = theme_config::load('boost');
+    return theme_boost_get_extra_scss($theme);*/
+}
+
+function theme_mwar_get_main_scss_content($theme) {
+    global $CFG;
+
+    $scss = '';
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
+    $fs = get_file_storage();
+    $context = context_system::instance();
+
+    if ($filename == 'default.scss') {
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    } else if ($filename == 'plain.scss') {
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_mwar', 'preset', 0, '/', $filename))) {
+        $scss .= $presetfile->get_content();
+    } else {
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
+    }
+
+    // Add 2 scss file to the beginning and end of main
+    $pre = file_get_contents($CFG->dirroot . '/theme/mwar/scss/pre.scss');
+    $fa = file_get_contents($CFG->dirroot . '/theme/mwar/scss/fontawesome/font-awesome.scss');
+    $post = file_get_contents($CFG->dirroot . '/theme/mwar/scss/post.scss');
+
+    return $pre . "\n" . $scss . "\n" . $post;
+}
+
+// Function to update an image loaded through the theme settings pages.
+function theme_mwar_update_settings_images($settingname) {
+
+    global $CFG;
+
+    // The setting name comes as a string like 's_theme_mwar_settingname'.
+    // Split it to get the actual setting name.
+    $parts = explode('_', $settingname);
+    $settingname = end($parts);
+
+    // Get context. Admin settings are stored in system context.
+    $syscontext = context_system::instance();
+    $component = 'theme_mwar';
+
+    // Filename of the uploaded file for the setting.
+    $filename = get_config($component, $settingname);
+
+    // Store file extension in a variable for further use.
+    $extension = substr($filename, strrpos($filename, '.') + 1);
+
+    // Path in the moodle file system
+    $fullpath = "/{$syscontext->id}/{$component}/{$settingname}/0{$filename}";
+
+    $fs = get_file_storage();
+
+    // Best way to get a file if we know the exact path.
+    if ($file = $fs->get_file_by_hash(sha1($fullpath))) {
+
+        // The stored file has been found --> copy it to dataroot in a location matched by the search for location in theme_config::resolve_image_location.
+        $pathname = $CFG->dataroot . '/pix_plugins/theme/mwar/' . $settingname . '.' . $extension;
+
+        // Retrieve any previous files with maybe different path extensions.
+        $pathpattern = $CFG->dataroot . '/pix_plugins/theme/mwar/' . $settingname . '.*';
+
+        // Make sure directory exists.
+        @mkdir($CFG->dataroot . '/pix_plugins/theme/mwar/', $CFG->directorypermissions, true);
+
+        // Delete any existing files for this setting.
+        foreach (glob($pathpattern) as $filename) {
+            @unlink($filename);
+        }
+
+        // Copy the new file to the specified location ($pathname).
+        $file->copy_content_to($pathname);
+    }
+    theme_reset_all_caches();
+}
+
--- settings.php
+++ settings.php
@@ -20,3 +20,125 @@
  * @copyright  2016 - Cellule TICE - Unversite de Namur
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingmwar', get_string('configtitle', 'theme_mwar'));
+    // General settings page.
+    $page = new admin_settingpage('theme_mwar_general', get_string('generalsettings', 'theme_mwar'));
+
+    // Scss variable overriding $brand-primary
+    $name = 'theme_mwar/brandcolor';
+    $title = get_string('brandcolor_desc', 'theme_mwar');
+    $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    $name = 'theme_mwar/preset';
+    $title = get_string('preset', 'theme_mwar');
+    $description = get_string('preset_desc', 'theme_mwar');
+    $default = 'default.scss';
+
+    // List preset files in our theme file area to add them to the dropdown choice list. We then add presets from boost.
+    $context = context_system::instance();
+    $fs = get_file_storage();
+    $files = $fs->get_area_files($context->id, 'theme_mwar', 'preset', 0, 'itemid, filepath', 'filename', false);
+    $choices = [];
+    foreach ($files as $file) {
+        $choices[$file->get_filename()] = $file->get_filename();
+    }
+    $choices['default.scss'] = 'default.scss';
+    $choices['plain.scss'] = 'plain.scss';
+
+    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Add file uploader to add preset files to our theme
+    $name = 'theme_mwar/presetfiles';
+    $title = get_string('presetfiles', 'theme_mwar');
+    $descriptin = get_string('presetfiles_desc', 'theme_mwar');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
+            array('maxfiles' => 20, 'accepted_types' => array('.scss')));
+    $page->add($setting);
+
+    // Add background logo to login page
+    $name = 'theme_mwar/loginbackgroundimage';
+    $title = get_string('loginbackgroundimage', 'theme_mwar');
+    $description = get_string('loginbackgroundimage_desc', 'theme_mwar');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'loginbackgroundimage');
+    $setting->set_updatedcallback('theme_mwar_update_settings_images');
+    $page->add($setting);
+    $settings->add($page);
+
+    // Advanced settings page.
+    $page = new admin_settingpage('theme_mwar_advanced', get_string('advancedsettings', 'theme_mwar'));
+
+    // Raw scss code to include before main content.
+    $name = 'theme_mwar/scsspre';
+    $title = get_string('rawscsspre', 'theme_mwar');
+    $description = get_string('rawscsspre_desc', 'theme_mwar');
+    $setting = new admin_setting_configtextarea($name, $title, $description, '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+
+    // Raw scss code to include after main content.
+    $name = 'theme_mwar/scss';
+    $title = get_string('rawscss', 'theme_mwar');
+    $description = get_string('rawscss_desc', 'theme_mwar');
+    $setting = new admin_setting_configtextarea($name, $title, $description, '', PARAM_RAW);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $page->add($setting);
+    $settings->add($page);
+
+    $page = new admin_settingpage('theme_mwar_toolselector', get_string('toolselector', 'theme_mwar'));
+
+    if (!$modules = $DB->get_records('modules', array('visible' => 1), 'name ASC')) {
+        print_error('moduledoesnotexist', 'error');
+    }
+
+    $activities = array();
+    $resources = array();
+
+    foreach ($modules as $module) {
+        $archetype = plugin_supports('mod', $module->name, FEATURE_MOD_ARCHETYPE, MOD_ARCHETYPE_OTHER);
+        $module->localname = get_string('pluginname', 'mod_' . $module->name);
+        if ($archetype !== MOD_ARCHETYPE_RESOURCE && $archetype !== MOD_ARCHETYPE_SYSTEM) {
+            $activities[] = $module;
+        } else if ($archetype == MOD_ARCHETYPE_RESOURCE) {
+            $resources[] = $module;
+        }
+    }
+
+    usort($activities, function($a, $b) {
+        return strcmp($a->localname, $b->localname);
+    });
+    usort($resources, function($a, $b) {
+        return strcmp($a->localname, $b->localname);
+    });
+
+    //Activities.
+    $page->add(new admin_setting_heading('activitieslist', get_string('activitieslist', 'theme_mwar'), get_string('activitieslistdesc', 'theme_mwar')));
+    foreach ($activities as $activity) {
+        $name = 'theme_mwar/' . $activity->name . 'inshortlist';
+        $title = $activity->localname;
+        $default = false;
+        $setting = new admin_setting_configcheckbox($name, $title, '', $default);
+        //$setting->set_updatedcallback('theme_reset_all_caches');
+        $page->add($setting);
+    }
+    // Resources.
+    $page->add(new admin_setting_heading('resourceslist', get_string('resourceslist', 'theme_mwar'), get_string('activitieslistdesc', 'theme_mwar')));
+    foreach ($resources as $resource) {
+        $name = 'theme_mwar/' . $resource->name . 'inshortlist';
+        $title = $resource->localname;
+        //$description = get_string('enablecategorycolorsdesc', 'theme_mwar');
+        $default = false;
+        $setting = new admin_setting_configcheckbox($name, $title, '', $default);
+        //$setting->set_updatedcallback('theme_reset_all_caches');
+        $page->add($setting);
+    }
+$settings->add($page);
+    //$ADMIN->add('theme_mwar', $settingspage);
+}
