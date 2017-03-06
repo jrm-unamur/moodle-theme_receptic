cedab8d563e1ac7cb5f9cb91e1bf48437ef5eda8
Jean-Roch Meurisse <jean-roch.meurisse@fundp.ac.be>
2017-03-06 11:22:13 +0100

    style quiz

---
+++ classes/output/mod_quiz_renderer.php
@@ -0,0 +1,81 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme UnamurUI mod_quiz_renderer file.
+ *
+ * @package    theme_unamurui
+ * @author     2016 Jean-Roch Meurisse
+ * @copyright  2016 University of Namur - Cellule TICE
+ *
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace theme_receptic\output;
+
+use mod_quiz_view_object;
+class mod_quiz_renderer extends \mod_quiz_renderer
+{
+    /**
+     * Override to separate message output from edit button output
+     * Generate a message saying that this quiz has no questions, with a button to
+     * go to the edit page, if the user has the right capability.
+     * @param object $quiz the quiz settings.
+     * @param object $cm the course_module object.
+     * @param object $context the quiz context.
+     * @return string HTML to output.
+     */
+    public function no_questions_message($canedit, $editurl) {
+        $output = '';
+        $output .= $this->notification(get_string('noquestions', 'quiz'));
+
+        return $output;
+    }
+
+    /**
+     * Override to always display edit button to priviledged users
+     * Work out, and render, whatever buttons, and surrounding info, should appear
+     * at the end of the review page.
+     * @param mod_quiz_view_object $viewobj the information required to display
+     * the view page.
+     * @return string HTML to output.
+     */
+    public function view_page_buttons(mod_quiz_view_object $viewobj) {
+        global $CFG;
+        $output = '';
+
+        if (!$viewobj->quizhasquestions) {
+            $output .= $this->no_questions_message($viewobj->canedit, $viewobj->editurl);
+        }
+
+        $output .= $this->access_messages($viewobj->preventmessages);
+        if ($viewobj->canedit) {
+            $output .= $this->single_button($viewobj->editurl, get_string('editquiz', 'quiz'), 'get');
+        }
+        if ($viewobj->buttontext) {
+            $output .= $this->start_attempt_button($viewobj->buttontext,
+                $viewobj->startattempturl, $viewobj->preflightcheckform,
+                $viewobj->popuprequired, $viewobj->popupoptions);
+        }
+
+        if ($viewobj->showbacktocourse) {
+            $output .= $this->single_button($viewobj->backtocourseurl,
+                get_string('backtocourse', 'quiz'), 'get',
+                array('class' => 'singlebutton'));
+        }
+
+        return $output;
+    }
+}
