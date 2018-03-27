8387f06190eee020a5f673bb8a2c6e757c9b05ab
Jean-Roch Meurisse <jean-roch.meurisse@unamur.be>
2018-03-27 09:33:06 +0200

    add missing lang var

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2018020116';
+$plugin->version = '2018020117';
 $plugin->requires = '2016070700';
 $plugin->component = 'theme_receptic';
 $plugin->dependencies = [
