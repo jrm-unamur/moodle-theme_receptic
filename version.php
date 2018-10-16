1c5ac22b15db05bd288a54cf8f8cc8cd889548b5
Jean-Roch Meurisse <jean-roch.meurisse@unamur.be>
2018-10-16 11:51:32 +0200

    bump up version number to update events declaration

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018020132;
+$plugin->version = 2018020143;
 $plugin->requires = 2016070700;
 $plugin->component = 'theme_receptic';
 $plugin->dependencies = [
